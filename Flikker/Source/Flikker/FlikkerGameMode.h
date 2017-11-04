076cbd835a53df3ff8df1f83984b764eada0eedf
TFlexSoom <tchilbert@live.com>
2017-11-04 00:58:42 -0700

    All the stuff excluding binaries and starter content. Report any problems.

---
+++ Flikker/Source/Flikker/FlikkerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlikkerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFlikkerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFlikkerGameMode();
+};
+
+
+
